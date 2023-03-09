16736061b601c897daf7326df1ffa69a35dcb419
Filip Velebit Samodol <filip.samodol@coreline.agency>
2023-03-09 18:42:36 +0100

    Login

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -2,6 +2,7 @@ import { asyncMiddleware } from '../middleware/async-middleware';
 import { Request, Response } from 'express';
 import { userRepository } from '../modules/user/user-repository';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 export class UserController {
     registerUser() {
@@ -19,6 +20,25 @@ export class UserController {
                 return res.status(201).send(newUser);
             } catch (error) {
                 return res.status(400).send({ message: 'Bad request' });
+            }
+        });
+    }
+    loginUser() {
+        return asyncMiddleware(async (req: Request, res: Response) => {
+            try {
+                const { email, password } = req.body;
+                const user = await userRepository.findOne({ email: email });
+                if (!user) {
+                    return res.status(401).send({ message: 'Invalid Email or Password' });
+                }
+                const validPassword = await bcrypt.compare(password, user.password);
+                if (!validPassword) {
+                    return res.status(401).send({ message: 'Invalid Email or Password' });
+                }
+                const token = jwt.sign({ _id: user._id.toString() }, 'privatekey', { expiresIn: '7d' });
+                return res.status(200).send({ data: token, message: 'Logged in succesfully' });
+            } catch (error) {
+                return res.status(500).send({ message: 'Internal server error' });
             }
         });
     }
--- src/routes/user-routes.ts
+++ src/routes/user-routes.ts
@@ -4,5 +4,6 @@ import { userController } from '../controllers/user-controller';
 const router = express.Router();
 
 router.post('/', userController.registerUser());
+router.post('/login', userController.loginUser());
 
 export const userRouter = router;
