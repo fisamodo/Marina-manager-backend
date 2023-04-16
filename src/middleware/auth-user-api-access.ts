ec4e5ebc95a921a2366b31f882b46d40b987169b
Filip Velebit Samodol <filip.samodol@coreline.agency>
2023-04-16 15:17:24 +0200

    Add admin users BE

--- src/middleware/auth-user-api-access.ts
+++ src/middleware/auth-user-api-access.ts
@@ -3,13 +3,13 @@ import { asyncMiddleware } from './async-middleware';
 import jwt from 'jsonwebtoken';
 import { Request, Response } from 'express';
 
-export function allowSignedInUsers() {
+export function checkIfUserIsLoggedIn() {
     return asyncMiddleware(async (req: Request, res: Response) => {
         try {
-            const { cookie } = req.cookies;
-            const { _id } = jwt.verify(cookie, 'privatekey') as any; //extract to env
-
+            const { token } = req.cookies;
+            const { _id } = jwt.verify(token, 'privatekey') as any; //extract to env
             const user = await userRepository.findOne({ _id: _id });
+            console.log('user: ', user);
             if (!user) {
                 res.clearCookie('token');
                 throw new Error('Unauthorized user');
